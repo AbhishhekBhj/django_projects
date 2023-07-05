aa39fc2fa0121e96d09bf220762196e523bb943f
AbhishhekBhj <abhujel322@gmail.com>
2023-07-05 07:44:14 +0545

    created form and did some db operations

---
+++ home/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    # context = {"variable": "AbhishekBhujel", "variable2": "Lucky"}
+    return render(request, "index.html")
+    # return HttpResponse("Welcome to Bhujel App Developers")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
